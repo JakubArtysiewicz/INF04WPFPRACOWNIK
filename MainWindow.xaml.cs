90133d964798783c49717e14d59576ecae99905e
jakub.artysiewicz_in <jakub.artysiewicz@infotech.edu.pl>
2026-09-16 10:39:44 +0200

    git wszystko

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,45 @@ namespace INF04WPFPRACOWNIK
             InitializeComponent();
         }
 
+        String wynik = "";
+        private void Generuj_Click(object sender, RoutedEventArgs e)
+        {
+            String wynik = "";
+            String znakiRandom = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String znakiRandomMale = "abcdefghijklmnopqrstuvwxyz";
+            String znakiRandomCyfry = "0123456789";
+            String znakiRandomSpecjalne = "!@#$%^&*()_+-=[]{}|;':\",./<>?";
+
+            String znakiDoWylosowania = "";
+            Random random = new Random();
+            if (int.TryParse(IloscZnakow.Text, out int IloscZnakowGit))
+            {
+                if (MaleiWielkie.IsChecked == true)
+                {
+                    znakiDoWylosowania += znakiRandom + znakiRandomMale;
+                }
+                if (Cyfry.IsChecked == true)
+                {
+                    znakiDoWylosowania += znakiRandomCyfry;
+                }
+                if (Specjalne.IsChecked == true)
+                {
+                    znakiDoWylosowania += znakiRandomSpecjalne;
+                }
+                for (int i = 0; i < IloscZnakowGit; i++)
+                {
+                    wynik += (char)znakiDoWylosowania[random.Next(znakiDoWylosowania.Length)];
+                }
+            MessageBox.Show(wynik);
+            }
+        }
+
+        private void Zatwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Dane pracownika:" + Imie.Text + " " + Nazwisko.Text + " " + Stanowisko.Text + " Hasło: " + wynik
+            );
+
+        }
     }
 }
